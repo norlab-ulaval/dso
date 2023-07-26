de75596ce31b16c3274f2ac639870d4fd001378b
ogamache <o.gamache2@gmail.com>
2023-07-26 17:58:13 -0400

    Added docker files and scripts

---
+++ docker/config.sh
@@ -0,0 +1,5 @@
+REPOS_NAME="dso"
+LOCAL_HOME=$HOME
+DOCKER_HOME="/home/user"
+REPOS_FOLDER="${LOCAL_HOME}/Documents/ICRA2024/repos/${REPOS_NAME}"
+DATA_FOLDER="${LOCAL_HOME}/Documents/ICRA2024/dataset/images/"
